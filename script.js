1b366a209b4d4f198602354c0229b0ddbba01173
sharlenelim21 <sharlenelim2004@gmail.com>
2025-09-16 14:38:24 +0800

    changes

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Auto-update footer year
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("year").textContent = new Date().getFullYear();
 });
 
-// Toggle responsive menu
 function myFunction() {
     const nav = document.getElementById("myTopnav");
     nav.classList.toggle("responsive");
